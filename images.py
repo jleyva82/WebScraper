4c09fe908bcb6a156e988c2774a855bcb4e12ff5
Jesus Leyva <jleyva82@gmail.com>
2019-03-12 22:04:56 -0700

    Add files via upload

--- images.py
+++ images.py
@@ -17,17 +17,26 @@ def StartSearch():
     r = requests.get("http://www.bing.com/images/search", params=params)
 
     soup = BeautifulSoup(r.text, "html.parser")
-    links = soup.findAll("a", {"class": "thumb"})
+    #links = soup.findAll("img")
+    links = soup.findAll("a", {"class": "thumb"}) #This class call out works but doesn't get you the 'right' images
 
     for item in links:
+
         img_obj = requests.get(item.attrs["href"])
-        print("Getting", item.attrs["href"])
         title = item.attrs["href"].split("/")[-1]
+        print("Getting", title, "\nFrom:", item.attrs["href"])
+        '''
+        img_obj = requests.get(item.attrs["src"])
+        title = item.attrs["src"].split("/")[-1]
+        print("Getting", title, "\nFrom:", item.attrs["src"])
+        '''
         try:
             img = Image.open(BytesIO(img_obj.content))
             img.save("./" + dir_name + "/" + title, img.format)
         except:
             print("Could not save image!")
+
+        print("\n")
 
     StartSearch()
 
